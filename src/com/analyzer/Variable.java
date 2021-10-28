702d2b0c8cfc0c697d2db913738f9b3e0a8329bc
Andres-XL <amovilla1103@gmail.com>
2021-10-27 21:49:04 -0500

    implement variable operations

--- src/com/analyzer/Variable.java
+++ src/com/analyzer/Variable.java
@@ -23,6 +23,25 @@ public class Variable extends EquationObject {
         this.exponent = exponent;
     }
 
+    public boolean canAdd(Variable otherVariable) {
+         return this.base.equals(otherVariable.base) && this.exponent == otherVariable.exponent;
+    }
+
+    public Variable add(Variable otherVariable) {
+        return new Variable(this.coefficient+otherVariable.coefficient, this.base, this.exponent);
+    }
+
+    public boolean canMultiply(Variable otherVariable) {
+        return this.base.equals(otherVariable.base) || this.base.equals("") || otherVariable.base.equals("");
+    }
+
+    public Variable multiply(Variable otherVariable) {
+        int newCoefficient = this.coefficient * otherVariable.coefficient;
+        String newBase = this.base.equals("")? otherVariable.base : this.base;
+        int newExponent = this.base.equals("") || otherVariable.base.equals("")? this.exponent * otherVariable.exponent : this.exponent + otherVariable.exponent;
+        return new Variable(newCoefficient, newBase, newExponent);
+    }
+
     @Override
     public String toString() {
         if (base.equals("")) {
