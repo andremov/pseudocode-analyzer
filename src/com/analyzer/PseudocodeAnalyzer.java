04f717fa237a08b33e1dcf4b78f0b626fb04a098
Andres-XL <amovilla1103@gmail.com>
2021-10-27 21:48:08 -0500

    add file reading and displaying

---
+++ src/com/analyzer/PseudocodeAnalyzer.java
@@ -0,0 +1,87 @@
+package com.analyzer;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PseudocodeAnalyzer {
+
+    static Window window;
+
+    String fileLocation;
+    ArrayList<String> fileLines;
+
+    public static void main(String[] args) {
+        new PseudocodeAnalyzer();
+    }
+
+    public PseudocodeAnalyzer() {
+        ActionListener browseAction = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                handleBrowseAction();
+            }
+        };
+        fileLines = new ArrayList<>();
+        window = new Window(browseAction);
+    }
+
+    private String convertLines(ArrayList<String> lines) {
+        StringBuilder result = new StringBuilder();
+        int numDigits = ("" + lines.size()).length();
+        for (int i = 0; i < lines.size(); i++) {
+            result.append(" ").append("[").append(formatNumber(i+1, numDigits)).append("]");
+            result.append("  ").append(lines.get(i)).append("\n");
+        }
+        return String.valueOf(result);
+    }
+
+    private String formatNumber(int inputNumber, int numDigits) {
+        StringBuilder resultNumber = new StringBuilder("" + inputNumber);
+        while(resultNumber.length() < numDigits) {
+            resultNumber.insert(0, " ");
+        }
+        return resultNumber.toString();
+    }
+
+    private void readFile(File inputFile) {
+        try {
+            Scanner reader = new Scanner(inputFile);
+            fileLocation = inputFile.getPath();
+            while (reader.hasNextLine()) {
+                fileLines.add(reader.nextLine());
+            }
+            reader.close();
+            solveFile(fileLines);
+            window.updateUI("  " + fileLocation, convertLines(fileLines));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(window, "No se pudo leer el archivo.");
+            e.printStackTrace();
+        }
+    }
+
+    private void handleBrowseAction() {
+        JFileChooser fileBrowsePopup = new JFileChooser();
+        int choice = fileBrowsePopup.showOpenDialog(window);
+        if (choice != JFileChooser.APPROVE_OPTION) return;
+        readFile(fileBrowsePopup.getSelectedFile());
+    }
+
+    private void solveFile(ArrayList<String> lines) {
+        int curLine = 0;
+        while (!lines.get(curLine).equals("Inicio")) {
+            curLine ++;
+        }
+        curLine ++;
+
+//        System.out.println(curLine);
+
+    }
+
+    private void solveBlock(ArrayList<String> lines) {
+
+    }
+}
---
+++ src/com/analyzer/Window.java
@@ -0,0 +1,99 @@
+package com.analyzer;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Window extends JFrame {
+
+    JLabel fileLocationLabel;
+    JButton browseBtn;
+    JTextArea fileDisplayArea;
+    JScrollPane scrollPane;
+
+    public Window(ActionListener browseAction) {
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setLayout(null);
+        setTitle("Pseudocode Analyzer");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        init(browseAction);
+
+        setVisible(true);
+    }
+
+    private int getPadding(String side) {
+        switch(side){
+            case "right":
+                return 15;
+            case "top":
+            case "bottom":
+            case "left":
+            default:
+                return 10;
+        }
+    }
+
+    private int getInnerWidth() {
+        int trueValue = super.getWidth();
+        return trueValue - getPadding("left") - getPadding("right");
+    }
+
+    private int getElementMargin() {
+        return 10;
+    }
+
+    private int getInnerHeight() {
+        int trueValue = super.getHeight();
+        return trueValue - getPadding("top") - getPadding("bottom");
+    }
+
+    private int getLocationFromRight(int targetLocation) {
+        return getInnerWidth() + getPadding("left") - targetLocation;
+    }
+
+    private void init(ActionListener browseAction) {
+
+        int elemPosY = getPadding("top");
+        int elemHeight = 40;
+
+        browseBtn = new JButton("Browse");
+        browseBtn.setSize(200,elemHeight);
+        browseBtn.setLocation(getLocationFromRight(200), elemPosY);
+        browseBtn.addActionListener(browseAction);
+        add(browseBtn);
+
+        fileLocationLabel = new JLabel("");
+        fileLocationLabel.setSize(getInnerWidth() - browseBtn.getWidth() - getElementMargin(),elemHeight);
+        fileLocationLabel.setLocation(getPadding("left"), elemPosY);
+        fileLocationLabel.setBackground(new Color(250,235,215));
+        fileLocationLabel.setBorder(new LineBorder(new Color(120,120,120)));
+        fileLocationLabel.setEnabled(false);
+        add(fileLocationLabel);
+
+        elemPosY += elemHeight + getElementMargin();
+        elemHeight = 450;
+
+        fileDisplayArea = new JTextArea("");
+        fileDisplayArea.setEnabled(false);
+        fileDisplayArea.setForeground(new Color(0,0,0));
+        fileDisplayArea.setDisabledTextColor(new Color(0,0,0));
+        fileDisplayArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
+
+        scrollPane = new JScrollPane(fileDisplayArea);
+        scrollPane.setBorder(new LineBorder(new Color(120,120,120)));
+        scrollPane.setSize(getInnerWidth(),elemHeight);
+        scrollPane.setLocation(getPadding("left"), elemPosY);
+        add(scrollPane);
+    }
+
+    public void updateUI(String fileLocation, String fileContents) {
+        fileLocationLabel.setText(fileLocation);
+        fileDisplayArea.setText(fileContents);
+    }
+}
